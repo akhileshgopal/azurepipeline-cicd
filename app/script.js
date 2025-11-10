8bc52a75aece28849312a3689d61a2d21d28c0b5
AKHILESH V A GOPAL <106224697+akhileshgopal@users.noreply.github.com>
2025-11-11 00:27:00 +0530

    Update script.js

--- app/script.js
+++ app/script.js
@@ -3,7 +3,7 @@ function celebrate() {
     const confetti = document.getElementById('confetti');
 
     // Display congratulatory message
-    message.textContent = 'Congratulations!';
+    message.textContent = 'Happy Learning!';
     
     // Show confetti animation
     confetti.classList.remove('hidden');
